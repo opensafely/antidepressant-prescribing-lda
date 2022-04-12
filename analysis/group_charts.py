3c62655443d6bbe27c979fb73adf28984dac24be
Christine Cunningham <5705329+ccunningham101@users.noreply.github.com>
2022-04-12 08:41:47 -0700

    Update to deciles-charts:v0.0.9
    Version of deciles chart action that allows for glob selection of which measures file to use. Only generate for practice measure files

--- analysis/group_charts.py
+++ analysis/group_charts.py
@@ -1,6 +1,7 @@
 import argparse
 import pathlib
 import re
+import glob
 
 import pandas
 
@@ -18,18 +19,12 @@ def _get_group_by(measure_table):
     return list(measure_table.columns[:-4])
 
 
-def get_measure_tables(path):
-    if not path.is_dir():
-        raise AttributeError()
-
-    for sub_path in path.iterdir():
-        if not sub_path.is_file():
-            continue
-
-        measure_fname_match = re.match(MEASURE_FNAME_REGEX, sub_path.name)
+def get_measure_tables(input_files):
+    for input_file in input_files:
+        measure_fname_match = re.match(MEASURE_FNAME_REGEX, input_file.name)
         if measure_fname_match is not None:
             # The `date` column is assigned by the measures framework.
-            measure_table = pandas.read_csv(sub_path, parse_dates=["date"])
+            measure_table = pandas.read_csv(input_file, parse_dates=["date"])
 
             # We can reconstruct the parameters passed to `Measure` without
             # the study definition.
@@ -71,6 +66,14 @@ def write_group_chart(group_chart, path):
     group_chart.savefig(path)
 
 
+def get_path(*args):
+    return pathlib.Path(*args).resolve()
+
+
+def match_paths(pattern):
+    return [get_path(x) for x in glob.glob(pattern)]
+
+
 def add_date_lines(plt, vlines):
     # TODO: Check that it is within the range?
     print(vlines)
@@ -86,10 +89,10 @@ def add_date_lines(plt, vlines):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-dir",
+        "--input-files",
         required=True,
-        type=pathlib.Path,
-        help="Path to the input directory",
+        type=match_paths,
+        help="Glob pattern for matching one or more input files",
     )
     parser.add_argument(
         "--output-dir",
@@ -107,11 +110,11 @@ def parse_args():
 
 def main():
     args = parse_args()
-    input_dir = args.input_dir
+    input_files = args.input_files
     output_dir = args.output_dir
     date_lines = args.date_lines
 
-    for measure_table in get_measure_tables(input_dir):
+    for measure_table in get_measure_tables(input_files):
         measure_table = drop_zero_denominator_rows(measure_table)
         chart = get_group_chart(measure_table, date_lines=date_lines)
         id_ = measure_table.attrs["id"]
