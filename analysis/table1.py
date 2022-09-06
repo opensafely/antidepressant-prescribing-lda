e090982975d04f4a3142c75cd992fca4e50bc1f3
Christine Cunningham <5705329+ccunningham101@users.noreply.github.com>
2022-09-06 15:25:38 -0700

    Table1 for joined measures file
    First pass at generating a Table1 from a joined measures file.
    Rows are formed by category and group and columns are formed based on
    user-specified strings.
    
    Currently only outputs numerator and denominator percentages and uses
    data from the start_date month.
    
    Further formatting/clean-up likely needed.

--- analysis/table1.py
+++ analysis/table1.py
@@ -1,69 +1,103 @@
 import pathlib
 import argparse
-import pandas as pd
+import pandas
+import fnmatch
 
-from config import start_date, demographics, lda_subgroups
+from config import start_date
 
 """
-Generate Table1 by demographics using measures files
-Allows redaction to be handled by the measures framework
-Assumes the measure has been generated over the population
-And that the measure file naming follows
-measure_{attribute}_{subgroup}_rate.csv
+Generate table1 from joined measures file.
+Group by category and group, and then further split into columns based on user
+provided column names
 """
 
 
-def get_month(input_dir, measure_attribute, d):
+def get_measure_tables(input_file):
+    measure_table = pandas.read_csv(input_file)
+
+    return measure_table
+
+
+def subset_table(measure_table, measures_pattern, measures_list, date):
     """
-    Use measures small number suppression for consistency with measures results
-    Get the first month
+    Given either a pattern of list of names, extract the subset of a joined
+    measures file based on the 'name' column
     """
-    path = input_dir / "measure_{}_all_breakdown_{}_rate.csv".format(
-        measure_attribute, d
-    )
 
-    try:
-        data = pd.read_csv(path)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Measure file for {path} did not exist")
+    measure_table = measure_table[measure_table["date"] == date]
 
-    try:
-        data = data[data["date"] == start_date]
-    except KeyError:
-        raise KeyError("Measure file did not have date column")
+    if measures_pattern:
+        measures_list = match_paths(measure_table["name"], measures_pattern)
+        if len(measures_list) == 0:
+            raise ValueError("Pattern did not match any files")
 
-    return data[[d, measure_attribute, "population"]].rename(
-        columns={
-            d: "Category",
-            f"{measure_attribute}": "{}".format(
-                (" ".join(measure_attribute.split("_")).title())
-            ),
-            "population": "Total",
-        }
-    )
+    if not measures_list:
+        return measure_table
+    return measure_table[measure_table["name"].isin(measures_list)]
 
 
 def transform_percentage(x):
-    return (
+    transformed = (
         x.astype(str)
         + " ("
         + (((x / x.sum()) * 100).round(0)).astype(str)
         + ")"
     )
+    transformed.name = f"{x.name} (%)"
+    return transformed
 
 
 def get_percentages(df):
+    """
+    Create a new column which has count (%) of group
+    After computation is complete, replace nan with "REDACTED" again
+    """
     percent = df.groupby(level=0).transform(transform_percentage)
-    return df.join(percent, rsuffix=" (%)")
+    percent.Numerator = percent.Numerator.replace("nan (nan)", "[REDACTED]")
+    percent.Denominator = percent.Denominator.replace(
+        "nan (nan)", "[REDACTED]"
+    )
+    percent = percent.rename(
+        columns={
+            "Numerator": "Numerator (%)",
+            "Denominator": "Denominator (%)",
+        }
+    )
+    return percent
+
+
+def title_format(df):
+    df.category = df.category.str.title()
+    df.columns = [x.title() for x in df.columns]
+    return df
+
+
+def match_paths(files, pattern):
+    return fnmatch.filter(files, pattern)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-dir",
+        "--input-file",
         required=True,
-        type=pathlib.Path,
-        help="Path to the input directory",
+        help="Path to single joined measures file",
+    )
+    measures_group = parser.add_mutually_exclusive_group(required=True)
+    measures_group.add_argument(
+        "--measures-pattern",
+        required=False,
+        help="Glob pattern matching one or more measures names for rows",
+    )
+    measures_group.add_argument(
+        "--measures-list",
+        required=False,
+        help="A list of one or more measure names for rows",
+    )
+    parser.add_argument(
+        "--column-names",
+        nargs="*",
+        help="Split measures with these names into separate columns",
     )
     parser.add_argument(
         "--output-dir",
@@ -71,30 +105,40 @@ def parse_args():
         type=pathlib.Path,
         help="Path to the output directory",
     )
-    parser.add_argument(
-        "--measure-attribute",
-        required=True,
-        help="Name of the measure attribute",
-    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    input_dir = args.input_dir
+    input_file = args.input_file
+    measures_pattern = args.measures_pattern
+    measures_list = args.measures_list
     output_dir = args.output_dir
-    measure_attribute = args.measure_attribute
+    columns = args.column_names
 
-    tables = []
-    demographic_subset = list(set(demographics) - set(lda_subgroups.keys()))
-    for d in demographic_subset:
-        data = get_month(input_dir, measure_attribute, d)
-        tables.append(data)
-    table1 = pd.concat(tables, keys=demographic_subset, names=["Attribute"])
-    # TODO: print warning if table is empty?
-    if not table1.empty:
-        table1 = get_percentages(table1)
-    table1.to_csv(output_dir / "table1.csv", index=True)
+    measure_table = get_measure_tables(input_file)
+    subset = subset_table(
+        measure_table, measures_pattern, measures_list, start_date
+    )
+
+    subset = title_format(subset)
+
+    table1 = pandas.DataFrame()
+    for column in columns:
+        sub = subset[subset.Name.str.contains(column)]
+        sub = sub.set_index(["Category", "Group"])
+        sub = sub[["Numerator", "Denominator"]]
+        sub = sub.apply(pandas.to_numeric, errors="coerce")
+        sub = get_percentages(sub)
+        sub.columns = pandas.MultiIndex.from_product(
+            [[f"{column.title()}"], sub.columns]
+        )
+        if table1.empty:
+            table1 = sub
+        else:
+            table1 = table1.join(sub)
+
+    table1.to_html(output_dir / "table1.html", index=True)
 
 
 if __name__ == "__main__":
