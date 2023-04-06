23d36dc95bc828e1980615e4a25fae4d1fc6572e
Christine Cunningham <5705329+ccunningham101@users.noreply.github.com>
2023-04-06 11:36:41 +0100

    Apply suppression by date and groupby
    Applying suppression at the measures file level works for values<=5, but
    would not correctly redact the next smallest number if the total
    redacted is less than 5.
    
    Group the measures by date and all but 1 groupby. Add tests to ensure
    suppression is applied correctly to a whole file with multiple dates and
    multiple groupbys.
    
    Fix pandas warnings by using dataframe copies where appropriate.

--- analysis/join_and_round.py
+++ analysis/join_and_round.py
@@ -15,43 +15,27 @@ def _check_for_practice(table):
         )
 
 
-def _round_column(column, base=10):
-    """
-    Rounds values to nearest multiple of base.
-    Args:
-        x: Value to round
-        base: Base to round to
-        Returns:
-            Rounded value
-    """
-    return (column / base).round() * base
+def _suppress_column(column, small_number_threshold=5, redact_zeroes=False):
+    small_value_filter = (column > 0) & (column <= small_number_threshold)
+    large_value_filter = column > small_number_threshold
 
-
-def _suppress_column(
-    column, data, small_number_threshold=5, redact_zeroes=False
-):
-    small_value_filter = (data[column] > 0) & (
-        data[column] <= small_number_threshold
-    )
-    large_value_filter = data[column] > small_number_threshold
-
-    num_small_values = data.loc[small_value_filter, column].count()
-    num_large_values = data.loc[large_value_filter, column].count()
+    num_small_values = small_value_filter.sum()
+    num_large_values = large_value_filter.sum()
 
     # Redact zeroes before any return
     if redact_zeroes:
-        data.loc[data[column] == 0, column] = numpy.nan
+        column.loc[column == 0] = numpy.nan
 
     if num_small_values == 0:
-        return
+        return column
 
-    small_value_total = data.loc[small_value_filter, column].sum()
-    data.loc[small_value_filter, column] = numpy.nan
+    small_value_total = column.loc[small_value_filter].sum()
+    column.loc[small_value_filter] = numpy.nan
 
     if small_value_total > small_number_threshold:
-        return
+        return column
     if num_large_values == 0:
-        return
+        return column
 
     # If the total suppressed is small then reidentification may
     # be possible by comparing the total of the unsuppressed
@@ -60,23 +44,55 @@ def _suppress_column(
     # rows with the next smallest value, it may be possible to
     # change the query to reorder the rows and thus reveal their
     # values; so we suppress all of them.
-    next_smallest_value = data.loc[large_value_filter, column].min()
-    all_next_smallest = data[column] == next_smallest_value
-    data.loc[all_next_smallest, column] = numpy.nan
+    next_smallest_value = column.loc[large_value_filter].min()
+    all_next_smallest = column == next_smallest_value
+    column.loc[all_next_smallest] = numpy.nan
+    return column
 
 
-def redact_df(measure_table, redact_zeroes=False):
-    _suppress_column("numerator", measure_table, redact_zeroes=redact_zeroes)
-    _suppress_column("denominator", measure_table, redact_zeroes=redact_zeroes)
-
-
-def round_df(measure_table, base=10):
-    measure_table.numerator = _round_column(measure_table.numerator, base=base)
-    measure_table.denominator = _round_column(
-        measure_table.denominator, base=base
+def redact_df_by_date(measure_table, redact_zeroes=False):
+    redacted = measure_table.copy()
+    groupby = ["date"]
+    if "group_1" in redacted.columns:
+        redacted.group_0.fillna("Unknown", inplace=True)
+        groupby += ["group_0"]
+    level = f"level_{len(groupby)}"
+    num = (
+        redacted.groupby(groupby)
+        .apply(
+            lambda x: _suppress_column(
+                x.numerator, redact_zeroes=redact_zeroes
+            )
+        )
+        .reset_index()
+        .set_index(level)["numerator"]
+        .sort_index()
     )
-    measure_table.value = measure_table.numerator / measure_table.denominator
-    return measure_table
+    denom = (
+        redacted.groupby(groupby)
+        .apply(
+            lambda x: _suppress_column(
+                x.denominator, redact_zeroes=redact_zeroes
+            )
+        )
+        .reset_index()
+        .set_index(level)["denominator"]
+        .sort_index()
+    )
+    redacted.numerator = num
+    redacted.denominator = denom
+    return redacted
+
+
+def round_df(measure_table, cols, base=10):
+    """
+    Rounds counts to nearest multiple of base
+    And recomputes the value
+    """
+    rounded = measure_table.copy()
+    rounded[cols] = (rounded[cols] / base).round() * base
+    rounded["value"] = rounded["numerator"] / rounded["denominator"]
+    return rounded
 
 
 def _reshape_data(measure_table):
@@ -115,7 +131,9 @@ def _reshape_data(measure_table):
             numerator = copy.columns[-3]
             # Assume last groupby is the count
             group_by_count = list(group_by[:-1]) + ["date"]
-            total = copy.groupby(group_by_count).sum()[numerator]
+            total = copy.groupby(group_by_count).sum(numeric_only=True)[
+                numerator
+            ]
             total.name = "denominator"
             copy = copy.set_index(group_by_count)
             copy = pandas.merge(copy, total, left_index=True, right_index=True)
@@ -169,13 +187,14 @@ def _redacted_string(measure_table):
     A group could have the name NaN, so apply to specific columns
     """
     REDACTED_STR = "[REDACTED]"
-    measure_table.numerator = measure_table.numerator.replace(
-        numpy.nan, REDACTED_STR
+    replacement_dict = {numpy.nan: REDACTED_STR}
+    measure_table = measure_table.replace(
+        {
+            "numerator": replacement_dict,
+            "denominator": replacement_dict,
+            "value": replacement_dict,
+        }
     )
-    measure_table.denominator = measure_table.denominator.replace(
-        numpy.nan, REDACTED_STR
-    )
-    measure_table.value = measure_table.value.replace(numpy.nan, REDACTED_STR)
     return measure_table
 
 
@@ -269,15 +288,15 @@ def main():
         raise FileNotFoundError("No files matched the input pattern provided")
 
     tables = []
+    cols = ["numerator", "denominator"]
     for measure_table in get_measure_tables(input_list or input_files):
         table = _reshape_data(measure_table)
         if not skip_redaction:
-            redact_df(table, redact_zeroes=(not keep_zeroes))
+            table = redact_df_by_date(table, redact_zeroes=(not keep_zeroes))
         if not skip_round:
-            table = round_df(table, base=round_to)
+            table = round_df(table, cols, base=round_to)
         redacted_str = _redacted_string(table)
         tables.append(redacted_str)
-
     output = _join_tables(tables)
     _check_for_practice(output)
     write_table(output, output_dir, output_name)
--- tests/test_join_and_round.py
+++ tests/test_join_and_round.py
@@ -1,60 +1,86 @@
-from join_and_round import redact_df, round_df
+from join_and_round import _suppress_column, round_df, redact_df_by_date
 import pandas
 import pytest
 
 
 @pytest.fixture
-def test_data():
+def test_series():
+    return pandas.Series([0, 2, 6, 6, 7, 7, 11, 17, 21])
+
+
+@pytest.fixture
+def test_df():
+    """
+    Redaction should happen per date. If there are multiple groupbys, it
+    should happen per groupby, so create a dataframe in which if the
+    series were treated as one, it would not need additional redaction
+    """
+    dates = 4 * [pandas.to_datetime("2022-01-01")] + 4 * [
+        pandas.to_datetime("2022-02-01")
+    ]
     df = pandas.DataFrame(
         {
-            "numerator": [0, 2, 6, 6, 7, 7, 11, 17, 21],
-            "denominator": [11, 19, 22, 28, 33, 37, 44, 46, 55],
+            "date": dates,
+            "numerator": [2, 21, 22, 23, 4, 24, 25, 26],
+            "denominator": [35, 45, 82, 120, 49, 52, 129, 148],
         }
     )
-    df["value"] = df.numerator / df.denominator
+    df["value"] = df["numerator"] / df["denominator"]
     return df
 
 
-def test_redact_df_keep_zeroes(test_data):
+def test_suppress_keep_zeroes(test_series):
     """
     Check there are no numbers less than 5
     Check that all 6's get redacted as well
     But retain true zeroes
     """
-    redact_df(test_data, redact_zeroes=False)
-    not_redacted = ((test_data > 0) & (test_data <= 5)).any()
-    assert not not_redacted.numerator
-    assert not not_redacted.denominator
+    _suppress_column(test_series, redact_zeroes=False)
+    assert not ((test_series > 0) & (test_series <= 6)).any()
 
 
-def test_redact_df_and_zeroes(test_data):
+def test_suppress_6_and_zeroes(test_series):
     """
     Check there are no numbers less than 5
     Check that all 6's get redacted as well
     Check that there are no zeroes
     """
-    redact_df(test_data, redact_zeroes=True)
-    not_redacted = (test_data <= 5).any()
-    assert not not_redacted.numerator
-    assert not not_redacted.denominator
+    _suppress_column(test_series, redact_zeroes=True)
+    assert not (test_series <= 6).any()
 
 
-def test_redact_df_and_zeroes(test_data):
+def test_suppress_only_zeroes(test_series):
     """
     Check that zeroes get redacted even if there is no other redaction
     """
-    test_data.loc[1, "numerator"] = 9
-    redact_df(test_data, redact_zeroes=True)
-    not_redacted = (test_data <= 5).any()
-    assert not not_redacted.numerator
-    assert not not_redacted.denominator
+    test_series.loc[1] = 9
+    _suppress_column(test_series, redact_zeroes=True)
+    assert not (test_series <= 5).any()
 
 
-def test_round_df(test_data):
+def test_round_df(test_df):
     """
     Check that all values are divisible by 10
     """
-    rounded = round_df(test_data)
-    is_rounded = (rounded.mod(10) == 0).all()
-    assert is_rounded.numerator
-    assert is_rounded.denominator
+    cols = ["numerator", "denominator"]
+    rounded = round_df(test_df, cols)
+    assert (rounded[cols].mod(10) == 0).all(None)
+    assert (test_df.value != rounded.value).all()
+
+
+def test_redact_df(test_df):
+    """
+    Check that redaction happens per-date
+    """
+    redacted = redact_df_by_date(test_df, redact_zeroes=True)
+    assert redacted.numerator.isna().sum() == 4
+
+
+def test_redact_df_with_groups(test_df):
+    """ """
+    # Set all to tbe same date
+    test_df["date"] = test_df.date[0]
+    test_df["group_0"] = test_df.index // 4
+    test_df["group_1"] = test_df.index % 4
+    redacted = redact_df_by_date(test_df, redact_zeroes=True)
+    assert redacted.numerator.isna().sum() == 4
